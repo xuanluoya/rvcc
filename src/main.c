ddc40856176659e6c1234b9dc0cbfa9db3b444cc
Aislin <72391548+Xuanluo-Qiu@users.noreply.github.com>
2025-05-23 17:46:44 +0800

    feat: supported `+` and `-` operators

--- src/main.c
+++ src/main.c
@@ -17,19 +17,48 @@
 int
 main (int argc, char *argv[])
 {
-  // Error handing.
   if (argc != 2)
     {
       fprintf (stderr, "%s: invalid number of arguments", argv[0]);
       return 1;
     }
 
+  // `p` save the script from command line.
+  char *p = argv[1];
+
   // Declare a main segment.
   printf ("  .globl main\n");
   // main section label.
   printf ("main:\n");
   // li is aliases of addi, load a immediate into the register.
-  printf ("  li a0, %d\n", atoi (argv[1]));
+  // `strtol` will find the first number.
+  printf ("  li a0, %ld\n", strtol (p, &p, 10));
+
+  // analysis `p`.
+  while (*p)
+    {
+      if (*p == '+')
+        {
+          // skip symble
+          ++p;
+          // addi rd, rs1, imm => rd = rs1 + imm
+          printf ("  addi a0, a0, %ld\n", strtol (p, &p, 10));
+          continue;
+        }
+      else if (*p == '-')
+        {
+          ++p;
+          // rd = rs1 + (-imm)
+          printf ("  addi a0, a0, -%ld\n", strtol (p, &p, 10));
+          continue;
+        }
+      else
+        {
+          fprintf (stderr, "unexpected character: `%c`\n", *p);
+          return 1;
+        }
+    }
+
   // ret is aliases od `jalr x0, x1, 0`,used of return subroutines
   printf ("  ret\n");
 
